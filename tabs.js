ecaa17b4883226bfd6b52554e33cce4b6b9914ab
SalvadorMD <contacto@salvadormacias.com.mx>
2021-12-04 18:48:11 -0600

    refactorice tabs.js

--- tabs.js
+++ tabs.js
@@ -12,28 +12,24 @@ function changeTabFocus(e) {
   const keydownLeft = 37;
   const keydownRight = 39;
 
-  //change the tabindex of the current tab to -1
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute("tabindex", -1);
-  }
 
-  //if the right key is pushed, move to the next tab on the right
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
     }
+  
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
   }
-  //if the left key is pushed, move to the next tab on the left
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
-    }
-  }
-
-  tabs[tabFocus].setAttribute("tabindex", 0);
-  tabs[tabFocus].focus();
 }
 
 function changeTabPanel(e) {
@@ -47,21 +43,22 @@ function changeTabPanel(e) {
   tabContainer
     .querySelector('[aria-selected="true"]')
     .setAttribute("aria-selected", false);
-
+  
   targetTab.setAttribute("aria-selected", true);
 
-  mainContainer
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((panel) => panel.setAttribute("hidden", true));
+  hideContent(mainContainer, '[role="tabpanel"]');
+  showContent(mainContainer, [`#${targetPanel}`]);
 
-  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+  hideContent(mainContainer, 'picture');
+  showContent(mainContainer, [`#${targetImage}`]);
+}
 
-  mainContainer
-    .querySelectorAll('picture')
-    .forEach((picture) => picture.setAttribute("hidden", true));
+function hideContent(parent, content) {
+  parent
+  .querySelectorAll(content)
+  .forEach((item) => item.setAttribute("hidden", true));
+}
 
-  mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
-
-
-  // console.log(mainContainer);
+function showContent(parent, content) {
+  parent.querySelector([content]).removeAttribute('hidden');
 }
